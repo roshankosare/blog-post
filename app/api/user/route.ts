dd702e767e1a232120842a50368a00f3bae2f7a8
Roshan Kosare <roshankosarepro@gmail.com>
2023-11-01 16:15:51 +0530

    fix:user route module error

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,5 @@
+import { NextResponse } from "next/server";
 
-export async function GET(req:Request){
-    
+export async function GET(req: Request) {
+  return NextResponse.json({ message: "hello" });
 }
